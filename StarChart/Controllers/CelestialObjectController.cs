540f3ac5edec338dda557b1ae2c6d9bf6ac45f89
Alicina Memar <memaralicina@gmail.com>
2018-10-09 14:12:17 -0400

    Fixing last push 3

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using StarChart.Data;
+using System.Linq;
 
 namespace StarChart.Controllers
 {
@@ -8,11 +9,24 @@ namespace StarChart.Controllers
   [ApiController]
   public class CelestialObjectController : ControllerBase
   {
-    private readonly ApplicationDbContext _context;
 
+    private readonly ApplicationDbContext _context;
     public CelestialObjectController(ApplicationDbContext thecontext)
     {
       _context = thecontext;
+    }
+
+    [HttpGet("{id:int}", Name = "GetById")]
+    public IActionResult GetById(int id)
+    {
+      var celestialObject = _context.CelestialObjects.Find(id);
+
+      if(celestialObject == null)
+        return NotFound();
+
+      celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == id).ToList();
+
+      return Ok(celestialObject);
     }
 
   }
