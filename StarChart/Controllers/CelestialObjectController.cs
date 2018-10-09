a5a863a8801337349354fa825c02f9fe5ec539d9
Alicina Memar <memaralicina@gmail.com>
2018-10-09 13:33:16 -0400

    Errors fixed, attribute had to be of the class itself

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace StarChart.Controllers
+{
+  [Route("")]
+  [ApiController]
+  public class CelestialObjectController : ControllerBase
+  {
+
+  }
+}
